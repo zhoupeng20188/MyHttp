eaa88a6a273e2dc83dadd2fcefb97fec06be1a50
zhoupeng <287214764@qq.com>
2020-09-11 18:01:11 +0800

    Initial commit

---
+++ src/main/java/com/zp/MiaoshaTest2.java
@@ -0,0 +1,68 @@
+package com.zp;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * 秒杀测试1
+ * 使用lua脚本
+ * @Author zp
+ * @create 2020/9/11 16:33
+ */
+public class MiaoshaTest2 {
+    public static void main(String[] args) {
+        String url = "http://localhost:8003/lua/miaosha";
+        for (int i = 0; i < 1000; i++) {
+            String  urlNew = url + "?userId=" + i;
+            new Thread(()->doGet(urlNew)).start();
+        }
+
+    }
+
+    private static void doGet(String url) {
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet(url);
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+//            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+//                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
